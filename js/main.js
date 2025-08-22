6e3c7cd250c7cd239595652f2293dfb7022c2937
hectoraburto <hector.aburto@gmail.com>
2025-08-22 11:57:09 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -45,3 +45,8 @@ document.addEventListener('DOMContentLoaded', () => {
         currentYearSpan.textContent = new Date().getFullYear();
     }
 });
+// Toggle menú móvil
+document.getElementById('menu-toggle').addEventListener('click', function () {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('hidden');
+});
